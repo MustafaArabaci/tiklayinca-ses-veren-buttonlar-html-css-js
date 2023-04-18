8153ca98e114e8ace68c66ce80b611215c19d4b5
MustafaArabaci <mustafaarabaci2007@gmail.com>
2023-04-18 09:36:59 +0300

    Tıklayınca Ses Veren Buttonlar Html Css Js Egzoz Temalı

---
+++ 9.Tıklayınca Ses Gelme Egzoz/main.js
@@ -0,0 +1,28 @@
+const sounds = ["konya", "warex", "dereceli", "bomking","islik"];
+
+sounds.forEach((sound) => {
+   const btn = document.createElement('button');
+   btn.classList.add('btn')
+
+   btn.innerText = sound;
+   
+   btn.addEventListener('click', () => {
+   stopSound();
+
+      document.getElementById(sound).play()
+   })
+
+   document.getElementById("buttons").appendChild(btn);
+})
+
+function stopSound(){
+   sounds.forEach((sound) => {
+      const song = document.getElementById(sound);
+
+      song.pause()
+      song.currentTime = 0
+   })
+
+}
+
+
